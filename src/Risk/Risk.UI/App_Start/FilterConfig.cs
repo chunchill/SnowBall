f869668ce954f41351bc393c656f5394e261e79b
sean <xujihui1985@gmail.com>
2013-10-20 22:59:38 +0800

    init commit

---
+++ src/Risk/Risk.UI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Risk.UI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
